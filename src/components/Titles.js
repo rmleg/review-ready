4fca0658be62c3a119474fb126176681fcf269a6
Ryan Hoffer <ryanlhoffer@gmail.com>
2020-03-05 16:04:48 -0500

    Created a set containing keys of selected checkboxes

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,10 +1,44 @@
 import React from "react";
 
+const selectedTitles = new Set();
+
+const clickHandler = (index) => {
+    console.log(index)
+    if(selectedTitles.has(index)){
+        selectedTitles.delete(index)
+    }else{
+        selectedTitles.add(index)
+    }
+
+    console.log(selectedTitles)
+}
+
+const Title = (props) =>{
+    return(
+        <>
+            <li><input type="checkbox" onClick={()=>clickHandler(props.info.key)}/> {props.info.header}</li>
+        </>
+    )
+}
+
 const Titles = (props) => {
-    let output = props.data.map(title=>{
-    return <li>{title}</li>
-  })
-  
+const data=[]
+const keys =[];
+
+for (let index = 0; index < props.data.length; index++) {
+    const headerValue = props.data[index];
+    data.push({
+        key: index,
+        header: headerValue
+    })
+    
+}
+
+let output = data.map(info=>{
+    return <Title key={info.key} info={info}/>
+    //return <li key={info.key}><input type="checkbox" /> {info.header}</li>
+})
+
 return <div className="col-6"><ul>{output}</ul></div>
 };
 
