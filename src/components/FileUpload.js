5135473fc67296d1acbb595d45113932b75e1dda
Ryan Hoffer <ryanlhoffer@gmail.com>
2020-03-05 13:30:11 -0500

    Cleaned up FileUpload

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,21 +1,5 @@
 import React from "react";
 
-/* class FileUpload extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.onClickHandler}>
-        <label htmlFor="file">Upload CSV</label>
-        <input type="file" id="input" />
-        <button type="submit">Upload</button>
-      </form>
-    );
-  }
-} */
-
 const FileUpload = props => {
   return (
     <form onSubmit={props.onClickHandler}>
