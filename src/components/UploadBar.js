1d2a6455723983c195a772148c1c81af09d513bf
Ryan Hoffer <ryanlhoffer@gmail.com>
2020-03-05 11:14:39 -0500

    Form no longer posts, goes to a click handler instead

--- src/components/UploadBar.js
+++ src/components/UploadBar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+const onClickHandler =()=>{
+    console.log('Click Handler')
+}
+
 const UploadBar = () =>{
     return(
     <>
-        <form action="upload.php" method="post" enctype="multipart/form-data">
-            <input type="file" name="fileToUpload" id="fileToUpload" />
-            <input type="submit" value="Upload File" name="submit" />
-        </form>
+            <input type="file" name="file"/>
+            <button onClick={onClickHandler}>Upload</button>
     </>)
     
 }
