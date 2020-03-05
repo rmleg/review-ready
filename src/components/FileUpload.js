8eb59c340ec534e8c3559bf705f35fb3d59eecf1
Rachel Leggett <rleggett@umich.edu>
2020-03-05 14:27:04 -0500

    Fix bootstrap form classes

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -4,11 +4,13 @@ import { Button } from "reactstrap";
 const FileUpload = props => {
   return (
     <form className="row" onSubmit={props.onClickHandler}>
-      <label htmlFor="file">Upload CSV</label>
-      <input type="file" id="input" />
-      <Button color="info" type="submit">
-        Upload
-      </Button>
+      <div className="form-group">
+        <label htmlFor="file">Upload CSV</label>
+        <input type="file" id="input" className="form-control-file" />
+        <Button color="info" type="submit">
+          Upload
+        </Button>
+      </div>
     </form>
   );
 };
