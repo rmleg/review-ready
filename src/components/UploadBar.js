938edf6ed2cee717c576f0336ea2bbe4b3e2e76f
Ryan Hoffer <ryanlhoffer@gmail.com>
2020-03-05 11:37:26 -0500

    Click handler now gets file information appropriately.

--- src/components/UploadBar.js
+++ src/components/UploadBar.js
@@ -1,15 +1,18 @@
 import React from "react";
 
 const onClickHandler = e => {
-  console.log("Click Handler.");
   e.preventDefault();
+  const uploadedFile = document.getElementById('input').files[0];
+
+  console.log(uploadedFile)
+  
 };
 
 const UploadBar = () => {
   return (
     <form onSubmit={onClickHandler}>
       <label htmlFor="file">Upload CSV</label>
-      <input type="file" name="file" />
+      <input type="file" id="input" />
       <button type="submit">Upload</button>
     </form>
   );
