7becf035ce8370afe9d91a798b7b429dd8afa1ff
Rachel Leggett <rleggett@umich.edu>
2020-03-05 13:59:48 -0500

    Adds Bootstrap

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Button } from "reactstrap";
 
 const FileUpload = props => {
   return (
     <form onSubmit={props.onClickHandler}>
       <label htmlFor="file">Upload CSV</label>
       <input type="file" id="input" />
-      <button type="submit">Upload</button>
+      <Button color="info" type="submit">
+        Upload
+      </Button>
     </form>
   );
 };
