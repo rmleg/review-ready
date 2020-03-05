cf8ec1a28c6b7b40abdfdd416a8627454c04c059
Rachel Leggett <rleggett@umich.edu>
2020-03-05 12:26:29 -0500

    File type check and error message

--- src/App.js
+++ src/App.js
@@ -4,28 +4,34 @@ import "./App.scss";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-
-
 class App extends React.Component {
-  constructor(props){
-    super(props)
-    this.state={
-      uploadedFile: false
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      uploadedFile: false,
+      error: false
+    };
   }
 
-  fileUploadHandler = (file) =>{
-    
-    this.setState({
-      uploadedFile:file
-    })
-  }
-  
+  fileUploadHandler = file => {
+    if (file && file.type === "text/csv") {
+      this.setState({
+        uploadedFile: file,
+        error: false
+      });
+    } else {
+      this.setState({
+        error: true
+      });
+    }
+  };
+
   render() {
     return (
       <div className="App">
         <Header />
-        <FileUpload fileUploadHandler={this.fileUploadHandler}/>
+        {this.state.error ? <p>Upload a valid CSV file.</p> : null}
+        <FileUpload fileUploadHandler={this.fileUploadHandler} />
         <Footer />
       </div>
     );
