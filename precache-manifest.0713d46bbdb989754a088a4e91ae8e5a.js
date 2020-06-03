ad03c102216dac325c46850339f9cf04f796ecab
Rachel Leggett <rleggett@umich.edu>
2020-06-03 15:45:44 -0400

    Updates

---
+++ precache-manifest.0713d46bbdb989754a088a4e91ae8e5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860dd6413a7574bb09f95d1c6515f14c",
+    "url": "/review-ready/index.html"
+  },
+  {
+    "revision": "f3ba5b2d79453be116a5",
+    "url": "/review-ready/static/css/main.76310b0c.chunk.css"
+  },
+  {
+    "revision": "d746a061c15cbf6ff3ae",
+    "url": "/review-ready/static/js/2.9a19c299.chunk.js"
+  },
+  {
+    "revision": "dfc4dca97a2ea2a3cae9037487c45f96",
+    "url": "/review-ready/static/js/2.9a19c299.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3ba5b2d79453be116a5",
+    "url": "/review-ready/static/js/main.903ef144.chunk.js"
+  },
+  {
+    "revision": "5c57668c04703c3e65a0",
+    "url": "/review-ready/static/js/runtime-main.a53f1d5b.js"
+  },
+  {
+    "revision": "17e0464f108c04d63092c49769618d96",
+    "url": "/review-ready/static/media/review-ready-logo.17e0464f.png"
+  }
+]);
